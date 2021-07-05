e0e7495d54c708709c4ff37720011313bcc7589d
Roberval Filho <rslfilho@gmail.com>
2021-07-05 18:45:53 -0300

    Colocando as rotas dentro de um Switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
 import Users from './components/Users';
@@ -19,11 +19,14 @@ class App extends Component {
             <Link to="/users/1" >Users</Link>
           </li>
         </ul>
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-        <Route 
-          path="/users/:id" 
-          render={ (props) => <Users {...props} greetingsMessage="Good Morning" />} />
+        <Switch>
+          <Route 
+            path="/users/:id" 
+            render={ (props) => <Users {...props} greetingsMessage="Good Morning" />} 
+          />
+          <Route path="/about" component={ About } />
+          <Route exact path="/" component={ Home } />
+        </Switch>
       </BrowserRouter>
     );
   }
